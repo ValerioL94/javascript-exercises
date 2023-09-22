32b43e61449b3352b85918852e35451522422f90
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-09-22 12:58:21 +0200

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...args) {
+    const result = [];
+    array.forEach((value) => {
+        if (!args.includes(value)) {
+            result.push(value);
+        }
+    });
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
