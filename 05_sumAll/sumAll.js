996c5c99623e00981238e1cdce56c2d9a86d9d6c
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-09-23 12:30:45 +0200

    Complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,27 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+    let sum = 0
+    if (num1 < 0 || num2 < 0) return "ERROR";
+    if (!Number.isInteger(num1) || (!Number.isInteger(num2))) return "ERROR";
+    if (num1 > num2) {
+        const numTemp = num1;
+        num1 = num2;
+        num2 = numTemp;
+    }
+    for (i = num1; i <= num2; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+
+
+/* 
+create variable sum
+sort x < y
+x <= y, add x to sum, x++
+
+*/
