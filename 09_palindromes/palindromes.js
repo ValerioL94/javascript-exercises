c965afc50fc35ea696943359372e96524f48cd90
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-10-16 12:42:06 +0200

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    let reversedString = processedString.split("").reverse().join("");
+    return processedString == reversedString;
 };
 
 // Do not edit below this line
