ba5375f1cbda7ce9f64bc098fc1b13b7b3fcac70
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-10-16 13:47:03 +0200

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
 
+    let firstPrev = 1;
+    let secondPrev = 0;
+
+    for (let i = 2; i <= n; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// fibonacci 4 => 1, 1, 2, 3 => 3
