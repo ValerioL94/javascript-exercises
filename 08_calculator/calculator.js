fa3df2778e248222cabe32b41b3ad6e3ba5a1e72
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-10-16 12:07:09 +0200

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function (x, y) {
+  return (x + y);
 };
 
-const subtract = function() {
-	
+const subtract = function (x, y) {
+  return (x - y);
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((product, current) => product * current);
 };
 
-const power = function() {
-	
+const power = function (x, y) {
+  return (x ** y); // or return Math.pow(x, y)
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
